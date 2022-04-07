520f871a5b30c50e76d0303667a192c15e8ed5de
Guilherme Fonseca Leite <guilherme.leite1987@gmail.com>
2022-04-07 12:55:33 -0300

    Obtendo um post por id

---
+++ src/main/java/com/gfl1987/wsmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.gfl1987.wsmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gfl1987.wsmongo.domain.Post;
+import com.gfl1987.wsmongo.repository.PostRepository;
+import com.gfl1987.wsmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) { //metodo que retorna usuario por ID
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
